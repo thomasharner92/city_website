6e22db53272f4eebef0119de257e1484c1490710
thomasharner92 <thomasharner92@live.com>
2017-02-09 14:21:41 -0500

    Added AJAX logic
    AJAX request aded to read in the city data from the geojson file on the
    server.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
-*/ JS file for working with AJAX (Pre Modules) */
+/* JS file for working with AJAX (Pre Modules) */
 
 function jsAjax() {
+  
+    // Create the request
     var ajaxRequest = new XMLHttpRequest(); // set up comm with the server
-}
+    
+    // Create event handler to send received data to callback function
+    ajaxRequest.onreadystatechange = function() {
+        console.log("readyState: ", ajaxRequest.readyState);
+        if (ajaxRequest.readyState == 4){
+            callback(ajaxRequest.response);
+        };
+    };
+    
+    // Open Server CONN (Get geojson from the sever)
+    ajaxRequest.open('GET','data/cities.geojson',true)
+    
+    // Set Response data type (what type of data we expect to receive)
+    ajaxRequest.responseType = "json";
+    
+    ajaxRequest.send();
+
+};
+
+function callback(response){
+    //console.log(response);
+    console.log(JSON.stringify(response));
+};
 
 window.onload = jsAjax();
